45a8afacda7cdedf2f54a9815dda5af08650880a
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2020-01-21 16:40:28 +0100

    defaultJavaClassPath

--- src/main/java/soot/Scene.java
+++ src/main/java/soot/Scene.java
@@ -558,7 +558,11 @@ public class Scene // extends AbstractHost
     if (Options.v().src_prec() == Options.src_prec_apk) {
       return defaultAndroidClassPath();
     } else {
-      return defaultJavaClassPath();
+      String path = defaultJavaClassPath();
+      if (path == null) {
+        throw new RuntimeException("Error: cannot find rt.jar.");
+      }
+      return path;
     }
   }
 
@@ -642,7 +646,11 @@ public class Scene // extends AbstractHost
     return jarPath;
   }
 
-  private String defaultJavaClassPath() {
+  /**
+   * Returns the default class path used for this JVM.
+   * @return the default class path
+   */
+  public static String defaultJavaClassPath() {
     StringBuilder sb = new StringBuilder();
     if (System.getProperty("os.name").equals("Mac OS X")) {
       // in older Mac OS X versions, rt.jar was split into classes.jar and
@@ -674,7 +682,7 @@ public class Scene // extends AbstractHost
         sb.append(rtJar.getAbsolutePath());
       } else {
         // not in JDK either
-        throw new RuntimeException("Error: cannot find rt.jar.");
+        return null;
       }
     }
 
